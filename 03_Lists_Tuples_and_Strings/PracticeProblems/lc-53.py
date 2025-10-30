ae8a64c59cc92983081ed5121d9c6113a28d82e1
Arpreet <arpreet4114@gmail.com>
2025-10-30 14:41:44 +0530

    question updated!

--- 03_Lists_Tuples_and_Strings/PracticeProblems/lc-53.py
+++ 03_Lists_Tuples_and_Strings/PracticeProblems/lc-53.py
@@ -1,3 +1,12 @@
+# 🧩 Problem — LeetCode 53: Maximum Subarray
+# 📝 Problem Statement
+# Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+# A subarray is a contiguous part of an array.
+# Example:
+# Input: nums = [-2,1,-3,4,-1,2,1,-5,4] 
+# Output: 6
+# Explanation: [4,-1,2,1] has the largest sum = 6.
+
 class Solution:
     def maxSubArray(self,nums:list[int])-> int:
         curr_sum = 0# initialize current sum to 0
@@ -8,7 +17,7 @@ class Solution:
                 max_sum = curr_sum # update max sum
             if curr_sum < 0:# reset current sum to 0 if it becomes negative
                 curr_sum = 0# reset current sum
-        return max_sum# return the maximum subarray sum 
+        return max_sum# return the maximum subarray sum
 # Example usage:    
 nums = [-2,1,-3,4,-1,2,1,-5,4]
 sol = Solution()
